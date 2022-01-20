8f869723ba24a506f4aca87d40e3ccb39d0abc62
Diego.monteagudo <diego.monteagudo@globant.com>
2022-01-20 17:06:26 -0500

    Initial Commit

---
+++ XChangeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XChangeApp
+//
+//  Created by Diego Sebastian Monteagudo Diaz on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XChangeAppUITests/XChangeAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  XChangeAppUITestsLaunchTests.swift
+//  XChangeAppUITests
+//
+//  Created by Diego Sebastian Monteagudo Diaz on 1/20/22.
+//
+
+import XCTest
+
+class XChangeAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
